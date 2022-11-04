38baf2af44b812fdf31789d292f8ecc0c8bf0c46
Edwin Llanos <edwin@wordbox.ai>
2022-11-04 12:35:39 -0500

    feat(Splash-screen): Add splash screen

--- app/src/main/java/com/weather/app/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/weather/app/presentation/search/SearchScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,6 +34,11 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.weather.app.R
 import com.weather.app.domain.model.WeatherModel
 import com.weather.app.presentation.search.viewmodel.SearchViewModel
@@ -90,7 +96,32 @@ fun SearchBody(
                     ItemRow(weatherModel = listWeatherModel[index], onItemClicked = onItemClicked)
                 }
             }
+            val hideAnimation = hasFocus or listWeatherModel.isNotEmpty()
+            WeatherAnimation(
+                hideAnimation,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 30.dp)
+            )
         }
+    }
+}
+
+@Composable
+fun WeatherAnimation(hideAnimation: Boolean, modifier: Modifier = Modifier) {
+    if (hideAnimation.not()) {
+        val composition by rememberLottieComposition(
+            LottieCompositionSpec.RawRes(R.raw.weather_animation)
+        )
+        val progress by animateLottieCompositionAsState(
+            composition = composition,
+            iterations = LottieConstants.IterateForever
+        )
+        LottieAnimation(
+            modifier = modifier,
+            composition = composition,
+            progress = { progress },
+        )
     }
 }
 
